c95d834fc39de1d8f5d7e4b5e39438936ed2c729
Andri Tanuarto <andritanuarto@gmail.com>
2019-06-04 20:40:03 -0700

    Setting up environment and react, webpack, scss

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="this is app">
+      <p>React here and there adasdas! sdsd</p>
+    </div>
+  );
+};
+
+export default App;
