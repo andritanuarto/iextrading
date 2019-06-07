158fe7d7f27a1a02c2d75406eb34a045fd07c784
Andri Tanuarto <andritanuarto@gmail.com>
2019-06-06 23:45:15 -0700

    Modify svg.js and add README file

--- src/svg.js
+++ src/svg.js
@@ -1,10 +1,8 @@
 export const SVG = {
   SVG_IEX_LOGO:
-    `
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><title>iex</title><g id="Layer_1" data-name="Layer 1"><rect width="50" height="50" style="fill:#f9a11b"/></g><g id="Layer_2_copy" data-name="Layer 2 copy"><polygon points="41.32 8.68 8.68 8.68 8.68 28.32 21.68 28.32 21.68 41.32 41.32 41.32 41.32 8.68" style="fill:#ffd16f"/></g></svg>
-    `,
+    `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><title>iex</title><g id="Layer_1" data-name="Layer 1"><rect width="50" height="50" style="fill:#f9a11b"/></g><g id="Layer_2_copy" data-name="Layer 2 copy"><polygon points="41.32 8.68 8.68 8.68 8.68 28.32 21.68 28.32 21.68 41.32 41.32 41.32 41.32 8.68" style="fill:#ffd16f"/></g></svg>`,
   SVG_SEARCH:
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-        <path style="fill: #ffffff" d="M34.3 30.2h-2.2l-.8-.8c2.7-3.1 4.3-7.2 4.3-11.6C35.7 8 27.7 0 17.8 0S0 8 0 17.8s8 17.8 17.8 17.8c4.4 0 8.5-1.6 11.6-4.3l.8.8v2.2L43.9 48l4.1-4.1-13.7-13.7zm-16.5 0c-6.8 0-12.3-5.5-12.3-12.3S11 5.5 17.8 5.5 30.2 11 30.2 17.8s-5.5 12.4-12.4 12.4z"/>
-      </svg>`
+    <path style="fill: #ffffff" d="M34.3 30.2h-2.2l-.8-.8c2.7-3.1 4.3-7.2 4.3-11.6C35.7 8 27.7 0 17.8 0S0 8 0 17.8s8 17.8 17.8 17.8c4.4 0 8.5-1.6 11.6-4.3l.8.8v2.2L43.9 48l4.1-4.1-13.7-13.7zm-16.5 0c-6.8 0-12.3-5.5-12.3-12.3S11 5.5 17.8 5.5 30.2 11 30.2 17.8s-5.5 12.4-12.4 12.4z"/>
+    </svg>`
 }
